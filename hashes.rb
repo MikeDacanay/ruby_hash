2e5e521b9ec9b7cbb76143971d0a7b5794a2c6bf
Mike Dacanay <migd64@gmail.com>
2017-10-03 00:29:12 -0700

    testing out hash built in methods

---
+++ hashes.rb
@@ -0,0 +1,12 @@
+test_hash = {name: "Mike", status: "tired", level: 3}
+# puts test_hash
+#puts test_hash.delete(:name)
+
+# puts test_hash.empty?
+
+# test_hash = {}
+
+# puts test_hash.empty?
+
+puts test_hash.has_key?(:name)
+puts test_hash.has_key?(:ssssname)
